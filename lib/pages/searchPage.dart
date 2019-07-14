f49079f2db1d1cc564644b8f0a01999f4eaa1343
yechao <best_yc@163.com>
2019-07-14 20:11:50 +0800

    搜索功能

--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -8,6 +8,8 @@ import 'package:wanandroid_flutter/entity/hot_key_entity.dart';
 import 'package:wanandroid_flutter/http/httpUtil.dart';
 import 'package:wanandroid_flutter/res/colors.dart';
 
+import 'articleDetail.dart';
+
 List<ArticleDataData> articleDatas = List();
 List<HotKeyData> hotKeyDatas = List();
 String key = "";
@@ -25,23 +27,17 @@ class _SearchPageState extends State<SearchPage> {
   void initState() {
     super.initState();
 
-    getHttp();
+    getHttpByHoyKey();
   }
 
-  void getHttp() async {
+  void getHttpByHoyKey() async {
     try {
       var hotKeyResponse = await HttpUtil().post(Api.HOT_KEY);
       Map hotKeyMap = json.decode(hotKeyResponse.toString());
       var hotKeyEntity = HotKeyEntity.fromJson(hotKeyMap);
 
-      var data = {'k': key};
-      var articleResponse = await HttpUtil().post(Api.QUERY, data: data);
-      Map articleMap = json.decode(articleResponse.toString());
-      var articleEntity = ArticleEntity.fromJson(articleMap);
-
       setState(() {
         hotKeyDatas = hotKeyEntity.data;
-        articleDatas = articleEntity.data.datas;
       });
 
       //3个参数：上下文，搜索代理，关键词,其中前两个必传，query可选
@@ -60,7 +56,7 @@ class _SearchPageState extends State<SearchPage> {
     );
   }
 
-  void getHttpByKey() async {
+  void getHttpByKey(String key) async {
     try {
       var data = {'k': key};
       var articleResponse = await HttpUtil().post(Api.QUERY, data: data);
@@ -81,6 +77,21 @@ class MySearchDelegate extends SearchDelegate<String> {
   @override
   List<Widget> buildActions(BuildContext context) {
     return [
+      FlatButton(
+        child: Text(
+          "搜索",
+          style: TextStyle(fontSize: 18, color: YColors.color_fff),
+        ),
+        onPressed: () async {
+          var data = {'k': query};
+          var articleResponse = await HttpUtil().post(Api.QUERY, data: data);
+          Map articleMap = json.decode(articleResponse.toString());
+          var articleEntity = ArticleEntity.fromJson(articleMap);
+          articleDatas = articleEntity.data.datas;
+
+          showResults(context);
+        },
+      ),
       // 显示一个清除的按钮
       IconButton(
         icon: Icon(Icons.clear),
@@ -108,44 +119,127 @@ class MySearchDelegate extends SearchDelegate<String> {
   }
 
   /// 搜索结果
+  /// 源码
+  /// onSubmitted: (String _) {
+  ///              widget.delegate.showResults(context);
+  ///            },
   @override
   Widget buildResults(BuildContext context) {
-    return Text("搜索结果a");
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: articleDatas.length,
+        itemBuilder: (BuildContext context, int position) {
+          if (position.isOdd) Divider();
+          return GestureDetector(
+            child: Container(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(Icons.android),
+                  title: Text(
+                    articleDatas[position]
+                        .title
+                        .replaceAll("<em class='highlight'>", "【")
+                        .replaceAll("<\/em>", "】"),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Padding(
+                    padding: EdgeInsets.only(top: 10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 6),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                                color: YColors.colorPrimary, width: 1.0),
+                            borderRadius: BorderRadius.circular((20.0)), // 圆角度
+                          ),
+                          child: Text(articleDatas[position].superChapterName,
+                              style: TextStyle(color: YColors.colorAccent)),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 15),
+                          child: Text(articleDatas[position].author),
+                        ),
+                      ],
+                    ),
+                  ),
+                  trailing: Icon(Icons.chevron_right),
+                )),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ArticleDetail(
+                        title: articleDatas[position]
+                            .title
+                            .replaceAll("<em class='highlight'>", "")
+                            .replaceAll("<\/em>", ""),
+                        url: articleDatas[position].link)),
+              );
+            },
+          );
+        });
   }
 
   /// 搜索建议 调用的话就不能返回null
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Wrap(
-      spacing: 10.0, //两个widget之间横向的间隔
-      direction: Axis.horizontal, //方向
-      alignment: WrapAlignment.start, //内容排序方式
-      children: List<Widget>.generate(
-        hotKeyDatas.length,
-        (int index) {
-          return ActionChip(
-            //标签文字
-            label: Text(
-              hotKeyDatas[index].name,
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
-              ),
-            ),
-            //点击事件
-            onPressed: () {
-              query = hotKeyDatas[index].name;
-              key = query;
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "大家都在搜：",
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: YColors.color_666),
+          ),
+          SizedBox(height: 10),
+          Wrap(
+            spacing: 10.0, //两个widget之间横向的间隔
+            direction: Axis.horizontal, //方向
+            alignment: WrapAlignment.start, //内容排序方式
+            children: List<Widget>.generate(
+              hotKeyDatas.length,
+              (int index) {
+                return ActionChip(
+                  //标签文字
+                  label: Text(
+                    hotKeyDatas[index].name,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  //点击事件
+                  onPressed: () async {
+                    query = hotKeyDatas[index].name;
+                    key = query;
 
-              /// 请求数据
-              showResults(context);
-            },
-            elevation: 3,
-            backgroundColor: Color.fromARGB(180, Random().nextInt(255),
-                Random().nextInt(255), Random().nextInt(255)),
-          );
-        },
-      ).toList(),
+                    /// 请求数据
+                    var data = {'k': key};
+                    var articleResponse =
+                        await HttpUtil().post(Api.QUERY, data: data);
+                    Map articleMap = json.decode(articleResponse.toString());
+                    var articleEntity = ArticleEntity.fromJson(articleMap);
+
+                    articleDatas = articleEntity.data.datas;
+
+                    /// 显示结果
+                    showResults(context);
+                  },
+                  elevation: 3,
+                  backgroundColor: Color.fromARGB(180, Random().nextInt(255),
+                      Random().nextInt(255), Random().nextInt(255)),
+                );
+              },
+            ).toList(),
+          ),
+        ],
+      ),
     );
   }
 
