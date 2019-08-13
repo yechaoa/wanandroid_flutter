2f6bb84ad97ca24e62de25e0fdf7d2ca63e83339
yechao <best_yc@163.com>
2019-08-13 22:06:55 +0800

    http 优化

--- lib/http/httpUtil.dart
+++ lib/http/httpUtil.dart
@@ -35,7 +35,7 @@ class HttpUtil {
       },
       //请求的Content-Type，默认值是[ContentType.json]. 也可以用ContentType.parse("application/x-www-form-urlencoded")
       contentType: ContentType.json,
-      //表示期望以那种格式(方式)接受响应数据。接受三种类型 `json`, `stream`, `plain`, `bytes`. 默认值是 `json`,
+      //表示期望以那种格式(方式)接受响应数据。接受四种类型 `json`, `stream`, `plain`, `bytes`. 默认值是 `json`,
       responseType: ResponseType.plain,
     );
 
